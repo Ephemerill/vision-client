bc8e9d8a4c5ad105663dcb5149d0bd478b877e00
Mazecorn <167123758+Mazecorn@users.noreply.github.com>
2025-11-14 08:44:51 -0800

    formatting

--- vision-client.sh
+++ vision-client.sh
@@ -27,9 +27,9 @@ show_header() {
     echo '   \$  /   $$ |$$$$$$$  |$$ |\$$$$$$  |$$ |  $$ |      \$$$$$$  |$$ |$$ |\$$$$$$$\ $$ |  $$ | \$$$$  |   ⠘⣿⣿⣄⠀⠈⠛⠉⠀⣠⣾⡿⠋'
     echo '    \_/    \__|\_______/ \__| \______/ \__|  \__|       \______/ \__|\__| \_______|\__|  \__|  \____/    ⠀⠀⠈⠛⠿⠶⣶⡶⠿⠟⠉'
     echo ""
+    tput smam
     echo -e "  ${PURPLE}Big Brother Vision Client v0.13 (Parsing Logic)${NC}"
     echo ""
-    tput smam
 }
 
 # --- FUNCTIONS ---
